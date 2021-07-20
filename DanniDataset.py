40c762f21e9e91445d79c07e63789f6f0a76083b
Chen, Danni <dchen360@gatech.edu>
2021-07-20 10:19:07 -0400

    Update DanniDataset.py

--- DanniDataset.py
+++ DanniDataset.py
@@ -1,20 +1,15 @@
 # Generate Image Dataset for PyTorch
 # Danni Chen
-# 6/27/2021
+# 07/20/2021
 # Modified from: https://pytorch.org/tutorials/beginner/data_loading_tutorial.html
 # Modified from Felipe's Dataset
 
 # import torch
 from torch.utils.data import Dataset
 import torchvision.transforms as transforms
-# from skimage import io
-# import PIL.Image
 import os, re
 import numpy as np
 import pandas as pd
-## QC Metrics ##
-# from .FocusMetric import FocusMetric
-# from .GreyMetric import GreyMetric
 from torchvision.io import read_image
 
 class DanniDataset(Dataset):
@@ -26,24 +21,35 @@ class DanniDataset(Dataset):
         
         self.imageData_df = imageData_df
         self.image_dir = image_dir
-        self.transform = transform
         
-  
         ## Each dataframe stores location of the image relative to image directory and 
         ## Image directory: C:\Users\danni\OneDrive\Mini Project\Garbage Classification Dataset\MetalPaper_Test
         ## Relative path: metal\metal1.png
         ## For relative path, can remove anything in front of metal, relative path is the speficif image from the point of view of the image directory
         
         ## need to create a dataframe and save it as CSV, do it with pandas
-        ## dataframe has relative path and the image label (0 or 1)
-        ## 
+        ## dataframe has relative path and the image label (0:paper or 1:metal)
+        ## root_dir = r'C:\Users\danni\OneDrive\Mini Project\Garbage Classification Dataset\MetalPaper_Train'
         
-        ## Load metadata (path_to_csv or dataframe) ##
-#         if (type(imageDataFile) == pd.core.frame.DataFrame):
-#             imageData_df = imageDataFile.copy()
-#         else:
-#             imageData_df = pd.read_csv(imageDataFile)
-     
+        data_info = {}
+        rel_file_list = []
+        label_list = []
+
+        for dir_, _, files in os.walk(self.image_dir):
+            for file_name in files:
+                rel_dir = os.path.relpath(dir_, root_dir)
+                rel_file = os.path.join(rel_dir, file_name)
+                rel_file_list.append(rel_file)
+                label = rel_file[0:5]
+                if label == 'paper':
+                    label = '0'
+                else:
+                    label = '1'
+                label_list.append(label)
+         data_info['relative_path'] = rel_file_list
+         data_info['label'] = label_list
+
+         imageData_df = pd.DataFrame(data_info)
             
     def __len__(self):
         ## Number of rows of imageData dataframe ##
