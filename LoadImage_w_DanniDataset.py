827b39f6d26a65b4b89d956388a3573c3ed6efb3
Chen, Danni <dchen360@gatech.edu>
2021-07-20 11:19:31 -0400

    Update LoadImage_w_DanniDataset.py

--- LoadImage_w_DanniDataset.py
+++ LoadImage_w_DanniDataset.py
@@ -1,6 +1,18 @@
 import torch
 import os
 import matplotlib.pyplot as plt
-from Danni_Summer2021MIBLab import 
 from torchvision.transforms import ToTensor
 from torch.utils.data import DataLoader
+
+from DanniDataset import DanniDataset
+
+## What to put for the parameters? ##
+image_dataset = DanniDataset(parameters)
+
+## Do I need to get different image_dataset for train_image_loader and test_image_loader? ##
+image_loader = torch.utils.data.DataLoader(
+    image_dataset, batch_size=batch_size,
+    pin_memory=True,
+    shuffle=True,
+    num_workers=1
+)
