4674f67e55ff2ca4b8aa42e966bc02c1aa53bf95
Chen, Danni <dchen360@gatech.edu>
2021-07-20 11:40:49 -0400

    Update LoadImage_w_DanniDataset.py

--- LoadImage_w_DanniDataset.py
+++ LoadImage_w_DanniDataset.py
@@ -6,7 +6,7 @@ from torch.utils.data import DataLoader
 
 from DanniDataset import DanniDataset
 
-## What to put for the parameters? ##
+## What to put for the parameters? The image directory? ##
 image_dataset = DanniDataset(parameters)
 
 ## Customized dataset is for retrieving the data using data loader ##
@@ -32,5 +32,31 @@ plt.imshow(test_image)
 plt.title(f'Image at index {idx}. Class Label {class_label}')
 plt.show()
 
-## Start constructing the covolution layer as image_loader works properly ##
+## ---------------------------------------------------------------------------------------------------------------------------------##
 
+## Start constructing the covolution layer as image_loader works properly ##
+## Build the CNN classification model. ##
+## Why using 'relu' as the activation function?? How to optimize the hyper-parameters for the model??
+
+model = Sequential([
+    Conv2D(filters=32, kernel_size=3, padding='same', activation='relu', input_shape=(300, 300, 3)),
+    MaxPooling2D(pool_size=2),
+
+    Conv2D(filters=64, kernel_size=3, padding='same', activation='relu'),
+    MaxPooling2D(pool_size=2),
+    
+    Conv2D(filters=32, kernel_size=3, padding='same', activation='relu'),
+    MaxPooling2D(pool_size=2),
+    
+    Conv2D(filters=32, kernel_size=3, padding='same', activation='relu'),
+    MaxPooling2D(pool_size=2),
+
+    Flatten(),
+
+    Dense(64, activation='relu'),
+
+    Dense(6, activation='softmax')
+])
+
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
+model.fit(train_generator, epochs=100, steps_per_epoch=2276//32,validation_data=validation_generator, validation_steps=251//32)
