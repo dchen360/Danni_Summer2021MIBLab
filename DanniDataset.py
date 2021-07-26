3ed7b4c5601a1e07872956144b90d8061675f9f0
Chen, Danni <dchen360@gatech.edu>
2021-07-26 10:48:47 -0400

    Update DanniDataset.py

--- DanniDataset.py
+++ DanniDataset.py
@@ -21,39 +21,6 @@ class DanniDataset(Dataset):
         
         self.metadata_df = metadata_df
         self.image_dir = image_dir
-        
-        ## Each dataframe stores location of the image relative to image directory and the label of the image
-        ## image_dir: r'C:\Users\danni\OneDrive\Mini Project\Garbage Classification Dataset\MetalPaper_Train'
-        
-        ## To dos:
-        ## ! image_dir should be able to see all images ##
-        ## one more col in metadata_df, if training ##
-        ## !! Be in another separate jupyter notebook named Create_Metadata, saving as a csv
-        
-        ## Relative path: metal\metal1.png
-        ## Image label (0:paper and 1:metal)
-        ## For relative path, can remove anything in front of metal, relative path is the speficif image from the point of view of the image directory
-        
-        ## Creating a dictionary that has two keys: 'Relative_path' and 'Label' with two lists being the values
-        data_info = {}
-        rel_file_list = []
-        label_list = []
-
-        for dir_, _, files in os.walk(self.image_dir):
-            for file_name in files:
-                rel_dir = os.path.relpath(dir_, root_dir)
-                rel_file = os.path.join(rel_dir, file_name)
-                rel_file_list.append(rel_file)
-                label = rel_file[0:5]
-                if label == 'paper':
-                    label = '0'
-                elif label == 'metal':
-                    label = '1'
-                label_list.append(label)
-         data_info['Relative_path'] = rel_file_list
-         data_info['Label'] = label_list
-
-         metadata_df = pd.DataFrame(data_info)
             
     def __len__(self):
         ## Number of rows of imageData dataframe ##
